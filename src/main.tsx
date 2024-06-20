579efc455b2cf64db8224b2f48b96b1565804f61
Arslonbek <roziboyevarslon0110@gmail.com>
2024-06-20 17:28:27 +0500

    fixed problems

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,8 @@ import ReactDOM from 'react-dom/client'
 import Router from "./router/routes.tsx"
 import 'react-toastify/dist/ReactToastify.css';
 import './index.css'
-import Root from './router/index.tsx'
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
    <>
     <Router />
    </>
-  // </React.StrictMode>,
 )

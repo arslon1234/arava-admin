182fafaa0c247cbd044c40997378d9c54f3d5e42
Arslonbek <roziboyevarslon0110@gmail.com>
2024-06-20 17:02:43 +0500

    first commit-2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import { Outlet } from "react-router-dom"
 function App() {
   return (
     <>
-      <h1>salom</h1>
+     <Outlet/>   
     </>
   )
 }
---
+++ src/pages/sign-in/index.tsx
@@ -0,0 +1,10 @@
+
+const index = () => {
+  return (
+    <div>
+      <h1>Sign-in</h1>
+    </div>
+  )
+}
+
+export default index

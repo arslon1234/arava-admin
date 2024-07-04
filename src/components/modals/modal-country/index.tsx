d7e379809c07fe37f9bc864c0b61d5075447a263
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2024-07-03 20:24:40 -1200

    Country modal ->  mui form update antd form

--- src/components/modals/modal-country/index.tsx
+++ src/components/modals/modal-country/index.tsx
@@ -1,14 +1,11 @@
-import * as React from "react";
+import { useState } from "react";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import { toast } from "react-toastify";
-import * as Yup from "yup";
-import { Field, Formik, Form, ErrorMessage } from "formik";
-import { Button, TextField } from "@mui/material";
-import EditIcon from '@mui/icons-material/Edit';
+import EditIcon from "@mui/icons-material/Edit";
 
-import {useCountryStore} from "@store";
-
+import { useCountryStore } from "@store";
+import { ConfigProvider, Form, Input , Button } from "antd";
 
 const style = {
   position: "absolute" as "absolute",
@@ -17,69 +14,62 @@ const style = {
   transform: "translate(-50%, -50%)",
   width: 400,
   bgcolor: "background.paper",
-  border: "2px solid #2BC62B", 
+  border: "2px solid #2BC62B",
   boxShadow: 24,
   p: 4,
 };
 
-interface propsData{
+interface propsData {
   title: string;
   id?: number;
   data?: any;
 }
 
-export default function BasicModal({title , id , data}:propsData) {
+export default function BasicModal({ title, id, data }: propsData) {
   const { postDataCountry, updateDataCountry } = useCountryStore();
 
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
+  const [loader , setLoader] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
   /// my code start <-----------------------------
- 
 
-  const validationSchema = Yup.object().shape({
-    nameUz: Yup.string().required("Name is required"),
-    nameRu: Yup.string().required("Name is required"),
-
-    // parent_category_id: Yup.number().min(0, "must be at least greater than 0"),
-    // position: Yup.number().min(0, "must be at least greater than 0"),
-  });
-
-  const initialValues: any = {
-    nameUz: data?.nameUz || "", 
-    nameRu: data?.nameRu || "", 
-  };
-
-  const handelSubmit = async (value:any ) => {
-    if(!id){
+  const handelSubmit = async (value: any) => {
+    if (!id) {
       console.log(value);
-      
+      setLoader(true)
       const status = await postDataCountry(value);
       if (status === 200) {
-      toast.success("success full");
-      setTimeout(() => {
-        handleClose();
-        window.location.reload();
-      },1000)
+        toast.success("success full");
+        setLoader(false)
+        setTimeout(() => {
+          handleClose();
+          window.location.reload();
+        }, 1000);
       } else {
-       toast.error("Error :" + status);
-       handleClose();
+        toast.error("Error :" + status);
+        setLoader(false)
+        handleClose();
       }
-    }else{
+    } else {
       console.log(value);
-      
-      const updateData= {id:id, nameUz: value?.nameUz , nameRu: value?.nameRu}
+
+      const updateData = {
+        id: id,
+        nameUz: value?.nameUz,
+        nameRu: value?.nameRu,
+      };
       const status = await updateDataCountry(updateData);
       if (status === 200) {
-      toast.success("update success full"); 
-      setTimeout(() => {
-        handleClose();
-        window.location.reload();
-      },1000)
+        toast.success("update success full");
+        setTimeout(() => {
+          handleClose();
+          window.location.reload();
+        }, 1000);
       } else {
-       toast.error("Error :" + status);
-       handleClose();
+        toast.error("Error :" + status);
+        handleClose();
       }
     }
   };
@@ -88,24 +78,26 @@ export default function BasicModal({title , id , data}:propsData) {
 
   return (
     <div>
-      {
-        title == "post" ? 
+      {title == "post" ? (
         <button
-        onClick={handleOpen}
-        className="py-2 px-6 text-white font-semibold bg-[#008524] hover:bg-[#008124] active:bg-[#008524] duration-200 rounded-lg"
-      >
-        To add
-      </button> : 
-      <Button
-        color="inherit"
-        onClick={handleOpen}
-        sx={{ 
-          color: '#767676' // HEX formatida rang
-        }}
-      >
-        <EditIcon  />
-      </Button>
-      }
+          onClick={handleOpen}
+          className="py-2 px-6 text-white font-semibold bg-[#008524] hover:bg-[#008124] active:bg-[#008524] duration-200 rounded-lg"
+        >
+          To add
+        </button>
+      ) : (
+        <Button
+          color="inherit"
+          onClick={handleOpen}
+          style={{
+            color: "#767676",
+            border: "none" ,
+            boxShadow: "none",// HEX formatida rang
+          }}
+        >
+          <EditIcon />
+        </Button>
+      )}
       <Modal
         open={open}
         onClose={handleClose}
@@ -113,60 +105,69 @@ export default function BasicModal({title , id , data}:propsData) {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Formik
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-            onSubmit={handelSubmit}
+          <ConfigProvider
+            theme={{
+              token: {
+                colorPrimary: "#008524",
+              },
+              components: {
+                Input: {
+                  activeBorderColor: "#008524",
+                  activeShadow: "#008524",
+                  hoverBorderColor: "#008524",
+                },
+              },
+            }}
           >
-            <Form className=" max-w-[600px]  w-full flex flex-col gap-[12px]">
-              <h1 className="text-center mb-2 text-[26px] font-bold">
-                {
-                  title == "post"? "Add a country" : "Edit a country"
-                }
+            <Form
+              name="nest-messages"
+              onFinish={handelSubmit}
+              style={{
+                width: 330,
+                display: "flex",
+                flexDirection: "column",
+              }}
+              layout="vertical"
+            >
+              <h1 className="text-center mb-2 text-[23px] font-semibold">
+                {title == "post" ? "Add a country" : "Edit a country"}
               </h1>
-              <Field
-                as={TextField}
-                label="Country name Uz"
-                sx={{ "& input": { color: "#00000", fontSize: "20px" , height:18 } ,  }}
-                type="text"
-                name="nameUz"
-                className=" w-[100%]  mb-3 outline-none py-0"
-                helperText={
-                  <ErrorMessage
-                     name="nameUz"
-                     component="div"
-                     className="mb-3 text-red-500 text-center text-[18px] font-medium "
-                  />
-                }
-              />
-              <Field
-                as={TextField}
-                label="Country name Ru"
-                sx={{ "& input": { color: "#00000", fontSize: "20px" , height:18 } }}
-                type="text"
-                name="nameRu"
-                className=" w-[100%]  mb-3 outline-none py-0 "
-                helperText={
-                  <ErrorMessage
-                     name="nameRu"
-                     component="div"
-                     className="mb-3 text-red-500 text-center text-[18px] font-medium"
-                  />
-                }
-              />
-              
-              <Button
-                sx={{ fontSize: "16px", fontWeight: "600" ,backgroundColor: "#008524", "&:hover" :{background: "#008124"} }}
-                variant="contained"
-                type="submit"
-                className="w-[100%] py-3"
-              >
-                {
-                  title == "post"? "to add" : "to edit"
-                }
-              </Button>
+              <div>
+                {/*  name  uz*/}
+                <Form.Item
+                  name="nameUz"
+                  label="Country name uz"
+                  style={{ width: "100%" }}
+                  rules={[{ required: true }]}
+                  initialValue={data?.nameUz ? data.nameUz : ""}
+                >
+                  <Input style={{ width: "100%" }} size="large" />
+                </Form.Item>
+
+                {/*  name ru */}
+                <Form.Item
+                  name="nameRu"
+                  label="Country name ru"
+                  style={{ width: "100%" }}
+                  rules={[{ required: true }]}
+                  initialValue={data?.nameRu ? data.nameRu : ""}
+                >
+                  <Input style={{ width: "100%" }} size="large" />
+                </Form.Item>
+              </div>
+              <Form.Item>
+                    <Button
+                      type="primary"
+                      htmlType="submit"
+                      size="large"
+                      loading={loader}
+                      style={{ width: "100%" }}
+                    >
+                      Submit
+                    </Button>
+                  </Form.Item>
             </Form>
-          </Formik>
+          </ConfigProvider>
         </Box>
       </Modal>
     </div>

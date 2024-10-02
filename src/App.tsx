005c65989218fb95ebaa94e5d2f4d3d5c6ac17c2
Arslonbek <roziboyevarslon0110@gmail.com>
2024-10-02 10:30:47 +0500

    give me money

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import { Outlet } from "react-router-dom";
 
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
-
+import './app.css'
 const App = () => {
   return (
-    <I18nextProvider i18n={i18n}>
-    <div>
-      <Outlet />
+    // <I18nextProvider i18n={i18n}>
+    // <div>
+    //   <Outlet />
+    // </div>
+    // </I18nextProvider>
+    <div className="app">
+      <h1 style={{fontSize: "40px"}}>Money</h1>
     </div>
-    </I18nextProvider>
   );
 };
 

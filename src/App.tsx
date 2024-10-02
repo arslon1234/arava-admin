ef482e4c9db9a1e49e8850facc71a47f925e9d08
Arslonbek <roziboyevarslon0110@gmail.com>
2024-10-02 10:34:16 +0500

    give me money2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { Outlet } from "react-router-dom";
+// import { Outlet } from "react-router-dom";
 
-import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n';
+// import { I18nextProvider } from 'react-i18next';
+// import i18n from './i18n';
 import './app.css'
 const App = () => {
   return (

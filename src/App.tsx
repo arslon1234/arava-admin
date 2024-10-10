a9a84f207a08fddd9cb4ac6a4b87ff3c24576fc0
Arslonbek <roziboyevarslon0110@gmail.com>
2024-10-10 09:18:41 +0500

    it works

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-// import { Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
-// import { I18nextProvider } from 'react-i18next';
-// import i18n from './i18n';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
 import './app.css'
 const App = () => {
   return (
-    // <I18nextProvider i18n={i18n}>
-    // <div>
-    //   <Outlet />
-    // </div>
-    // </I18nextProvider>
-    <div className="app">
-      <h1 style={{fontSize: "40px"}}>Money</h1>
+    <I18nextProvider i18n={i18n}>
+    <div>
+      <Outlet />
     </div>
+    </I18nextProvider>
   );
 };
 

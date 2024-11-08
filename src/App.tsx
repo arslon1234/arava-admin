9ec6caf6c2f4c7ee19e6c124c5645f2c88af62e5
Arslonbek <roziboyevarslon0110@gmail.com>
2024-11-08 18:56:26 +0500

    app updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { Outlet } from "react-router-dom";
+// import { Outlet } from "react-router-dom";
 
-import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n';
-import './app.css'
+// import { I18nextProvider } from 'react-i18next';
+// import i18n from './i18n';
+// import './app.css'
 const App = () => {
   return (
-    <I18nextProvider i18n={i18n}>
-    <div>
-      <Outlet />
-    </div>
-    </I18nextProvider>
+    <h1 className="text-center my-5 text-[40px] text-[crimson]">Sayt ishlamayapti</h1>
+    // <I18nextProvider i18n={i18n}>
+    // <div>
+    //   <Outlet />
+    // </div>
+    // </I18nextProvider>
   );
 };
 
